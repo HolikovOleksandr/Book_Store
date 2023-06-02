fe3b840654551df8af45945143fc2d3b35e3a8f7
Oleksandr <sanyagolikov97@gmail.com>
2023-06-02 20:05:52 +0300

    Do: Some cosmetic changes in UI

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,15 +13,8 @@ namespace Book_Store.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
